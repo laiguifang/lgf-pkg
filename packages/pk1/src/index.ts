914980630530d5a5fcfc3d3515351ba49e2b6177
laiguifang <laiguifang@3weijia.com>
2023-08-29 17:32:59 +0800

    init

---
+++ packages/pk1/src/index.ts
@@ -0,0 +1,4 @@
+// pk1/src/index.ts
+export function pk1(): any {
+  console.log('I am pk1');
+}
---
+++ packages/pk2/src/index.ts
@@ -0,0 +1,9 @@
+// pk2/src/index.ts
+
+import {pk1} from '@lgf-pkg/pk1'
+
+function pk2() {
+  pk1()
+  console.log('I am pk2');
+}
+export default pk2;
